6e3f2876e277845aba3226fd94983a8ad2f5facf
AliALmasiZ <ali1385almasi@gmail.com>
2025-02-20 23:41:13 +0330

    HW0_Q3

--- HW0/HW0_Q1.java
+++ HW0/HW0_Q1.java
@@ -3,31 +3,31 @@ import java.util.*;
 /* STDID : 403100043 */
 public class HW0_Q1 {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        String inputLine = scanner.nextLine();
-        scanner.close();
+        final Scanner sc = new Scanner(System.in);
+        String inputLine = sc.nextLine();
+        sc.close();
         String[] words = inputLine.split("[- ]+");
 
-        for (String currentWord : words) {
-            if (currentWord.isEmpty()) continue;
-            System.out.printf("%d ", getLeftMatchCount(currentWord, words));
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+            System.out.printf("%d ", getLeftMatchCount(word, words));
         }
         System.out.println();
-        for (String currentWord : words) {
-            if (currentWord.isEmpty()) continue;
-            System.out.printf("%d ", getRightMatchCount(currentWord, words));
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+            System.out.printf("%d ", getRightMatchCount(word, words));
         }
         System.out.println();
 
     }
 
-    private static int getLeftMatchCount(String currentWord, String[] words) {
+    private static int getLeftMatchCount(String word, String[] words) {
         int count = 1;
         for (int i = 0; i < words.length; i++) {
-            if (count == currentWord.length()) break;
-            if (count > words[i].length() || words[i].equals(currentWord)) continue;
+            if (count == word.length()) break;
+            if (count > words[i].length() || words[i].equals(word)) continue;
 
-            String sub1 = currentWord.substring(0, count);
+            String sub1 = word.substring(0, count);
             String sub2 = words[i].substring(0, count);
 
             if (sub1.equals(sub2)) {
--- HW0/HW0_Q2.java
+++ HW0/HW0_Q2.java
@@ -1,8 +1,80 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class HW0_Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        sc.nextLine();
+        char[][] table = new char[n][n];
+        for (int i = 0; i < n; i++) {
+//            print(i);
+            String word = sc.nextLine();
+//            print(word);
+            table[i] = word.toCharArray();
+        }
+        int m = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < m; i++) {
+            String word = sc.nextLine();
+            if(isMatch(word, table)) System.out.println("FOUND");
+            else System.out.println("NOT FOUND");
+        }
     }
+
+    private static boolean isMatch(String target, char[][] table) {
+        for (int i = 0; i < table.length * table.length; i++) {
+            int row = i / table.length;
+            int col = i % table.length;
+            if (target.charAt(0) == table[row][col]) {
+                if (isMatchHorizontally(target, table, row, col)|| isMatchVertically(target, table, row, col)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private static boolean isMatchAll(String target, char[][] table, int row, int col) {
+        return isMatchHorizontally(target, table, row, col) || isMatchVertically(target, table, row, col);
+    }
+    private static boolean isMatchVertically(String target, char[][] table, int row, int col) {
+        char[] targetChar = target.toCharArray();
+        StringBuilder sbUp = new StringBuilder();
+        StringBuilder sbDown = new StringBuilder();
+        /* col checker */
+        for (int i = 0; i < targetChar.length; i++) {
+            if (row + targetChar.length - 1 >= table.length) break;
+            sbDown.append(table[row + i][col]);
+        }
+        for (int i = 0; i < targetChar.length; i++) {
+            if (row - targetChar.length < 0) break;
+            sbUp.append(table[row - i][col]);
+        }
+        return target.contentEquals(sbUp) || target.contentEquals(sbDown);
+    }
+
+    private static boolean isMatchHorizontally(String target, char[][] table, int row, int col) {
+        char[] targetChar = target.toCharArray();
+        StringBuilder sbLeft = new StringBuilder();
+        StringBuilder sbRight = new StringBuilder();
+        /* row checker */
+        for (int i = 0; i < targetChar.length; i++) {
+            if (col + targetChar.length - 1 >= table.length) break;
+            sbRight.append(table[row][col + i]);
+        }
+
+        for (int i = 0; i < targetChar.length; i++) {
+            if (col - targetChar.length < 0) break;
+            sbLeft.append(table[row][col - i]);
+        }
+        print(sbLeft.toString());
+        print(Arrays.toString(table[row]));
+        return target.contentEquals(sbRight) || target.contentEquals(sbLeft);
+    }
+    private static void print(String s) {
+        System.out.println("-" + s + "-");
+    }
+    private static void print(int s) {
+        System.out.println("-" + s + "-");
+    }
+
 }
